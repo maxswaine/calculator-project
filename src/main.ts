2bff4c70320a653817f66b279e6fa55a6496c05d
Max Swaine <maxswainework@gmail.com>
2023-07-27 22:37:40 +0100

    initial push, basic design and graphics for calculator done. not ts functional yet. organised files and sorted font

---
+++ src/main.ts
@@ -0,0 +1,6 @@
+import "./main.scss";
+
+// Write a function that determines whether a button has been pressed
+// All the buttons can go under a singular element and can use a forEach loop to determine whether they have been pressed
+// Change the DOM of the Output screen to display the number that has been placed
+// store a variable that converts the string into a number that returns the sum
